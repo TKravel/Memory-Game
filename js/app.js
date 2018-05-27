ee5611dd8cfc698cff2adef607c8b5224fcb6fcd
TKravel <38673691+TKravel@users.noreply.github.com>
2018-05-26 22:48:39 -0400

    Collect, shuffle and replace cards
    Feat: Collect, shuffle and replace cards
    
    This commit collects the cards off the dom and turns them into an array usable by shuffle function. The cards are shuffled and placed back on the DOM in their new order

---
+++ js/app.js
@@ -0,0 +1,41 @@
+const board = document.querySelector('.deck');
+let cardStack = document.querySelectorAll('.deck li');
+// Turn HTML Collection into Array
+let cards = Array.from(cardStack);
+// Shuffle Array
+shuffle(cards);
+
+let fragment = document.createDocumentFragment();
+// Append Array indexes to fragment
+for(i = 0; i < cards.length; i++){
+    fragment.append(cards[i]);
+};
+// Append fragment to page
+board.appendChild(fragment);
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
